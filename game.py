702d13900aa637ed5276e8ca812822d0264eadd9
Eric Chan <eachan10@gmail.com>
2022-02-17 12:40:15 -0800

    Add scuffed multiplayer
    not entirely sure how the server handles multiple games
    pretty sure if a bunch of clients connect and disconnect memory will be
    flooded because the games aren't removed
    I use innerHTML to modify a paragraph element not sure how much of an
    issue that is
    A win condition is not yet implemented

--- game.py
+++ game.py
@@ -17,6 +17,13 @@ class Vector:
             raise ValueError('brug')
         return Vector(self.x + other.x, self.y + other.y)
 
+    def __iadd__(self, other):
+        if not isinstance(other, Vector):
+            raise ValueError('bruh')
+        self.x += other.x
+        self.y += other.y
+        return self
+
     def __eq__(self, other):
         if not isinstance(other, Vector):
             raise ValueError('brug')
@@ -42,9 +49,25 @@ class SnakeGame:
         self.snake = [Vector()]
         self.snake_dir = Vector(1, 0)  # direction of head movement
         self.snake_last_movement = Vector(self.snake_dir.x, self.snake_dir.y)
-        # 2 element list, [x, y] for position of the food
         self.food = Vector(random.randint(0, self.size[0] - 1), random.randint(0, self.size[1] - 1))
+        self.food_dir = Vector(0, 0)
+        self.food_can_move = False
         self.alive = True
+
+    def set_food_dir(self, dir):
+        """
+        Update direction of movement based on input
+        'up' 'down' 'left' 'right'
+        """
+        with self.lock:
+            if dir == 'up':
+                self.food_dir.set(0, -1)
+            elif dir == 'down':
+                self.food_dir.set(0, 1)
+            elif dir == 'left':
+                self.food_dir.set(-1, 0)
+            elif dir == 'right':
+                self.food_dir.set(1, 0)
 
     def set_snake_dir(self, dir):
         """
@@ -71,10 +94,28 @@ class SnakeGame:
         Snake tail follows segment ahead
         If food is consumed increase length
         """
+        if not self.sid or not self.food_sid:
+            return False
         with self.lock:
+            w, h = self.size
+
             # move the head of the snake
             self.snake.append(self.snake[-1] + self.snake_dir)
             self.snake_last_movement.set(self.snake_dir.x, self.snake_dir.y)
+
+            # move the food
+            if self.food_can_move:
+                self.food += self.food_dir
+            self.food_can_move = not self.food_can_move
+
+            if self.food.x < 0:
+                self.x = 0
+            if self.food.x >= w:
+                self.food.x = w - 1
+            if self.food.y < 0:
+                self.food.y = 0
+            if self.food.y >= h:
+                self.food.y = h - 1
 
             # check if food not at head
             if self.snake[-1] != self.food:
@@ -92,9 +133,9 @@ class SnakeGame:
                     self.alive = False # dead
 
             # check for board boundaries
-            w, h = self.size
-            if head.x < 0 or head.x > w or head.y < 0 or head.y > h:
+            if head.x < 0 or head.x >= w or head.y < 0 or head.y >= h:
                 self.alive = False
+        return True
 
     def get_data(self):
         """Return dict to send data to client"""
@@ -104,7 +145,7 @@ class SnakeGame:
         d = {
             'snake': snake,
             'food': food,
-            'alive': self.alive,  # not implemented yet
+            'alive': self.alive,
             'width': w,
             'height': h,
         }
--- snake.py
+++ snake.py
@@ -1,7 +1,7 @@
 from game import SnakeGame
 
 from flask import Flask, render_template, request, session
-from flask_socketio import SocketIO, join_room, leave_room
+from flask_socketio import SocketIO, join_room, leave_room, emit
 
 from time import sleep
 import threading
@@ -19,13 +19,29 @@ def test():
 @socketio.on('connect')
 def on_connect(data):
     print(f'joined {request.sid}')
-    games.append(SnakeGame([40, 40], request.sid))
+    for game in games:
+        if not game.food_sid:
+            game.food_sid = request.sid
+            emit('food')
+            break
+        if not game.sid:
+            game.sid = request.sid
+            break
+    else:
+        print('new game')
+        games.append(SnakeGame([40, 40], request.sid))
+        emit('snake')
 
     join_room(request.sid)
 
 @socketio.event
 def disconnect():
     # leave room when a client disconnects
+    for game in games:
+        if game.sid == request.sid:
+            game.sid = None
+        if game.food_sid == request.sid:
+            game.food_sid == None
     room = session.get('room')
     leave_room(room)
     
@@ -35,12 +51,16 @@ def handle_input(data):
         if game.sid == request.sid:
             if game.alive:
                 game.set_snake_dir(data)
+        if game.food_sid == request.sid:
+            if game.alive:
+                game.set_food_dir(data)
 
 @socketio.on('start')
 def handle_start():
     for game in games:
         if game.sid == request.sid:
             game.start()
+            print('starting')
 
 def game_loop():
     while True:
@@ -48,8 +68,10 @@ def game_loop():
             # update all game states
             if game.alive:
                 game.move()
-                socketio.emit('game update', game.get_data(), room=game.sid)
-        sleep(0.1)
+                data = game.get_data()
+                socketio.emit('game update', data, room=game.sid)
+                socketio.emit('game update', data, room=game.food_sid)
+        sleep(0.2)
 
 if __name__ == '__main__':
     # socketio.start_background_task(game_loop)
