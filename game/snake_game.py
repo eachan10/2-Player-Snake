48faabcbd859db7dd4004fe383f6ae8f97208eb3
Eric Chan <eachan10@gmail.com>
2022-03-15 11:48:02 -0700

    Package game logic

--- game/snake_game.py
+++ game/snake_game.py
@@ -1,40 +1,9 @@
-# game logic
-
 import random
 import time
 
+from .vector import Vector
+
 from eventlet.semaphore import Semaphore
-
-(a,) = [1]
-
-
-class Vector:
-    __slots__ = ("x", "y")
-
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    def set(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other):
-        if not isinstance(other, Vector):
-            return NotImplemented
-        return Vector(self.x + other.x, self.y + other.y)
-
-    def __iadd__(self, other):
-        if not isinstance(other, Vector):
-            return NotImplemented
-        self.x += other.x
-        self.y += other.y
-        return self
-
-    def __eq__(self, other):
-        if not isinstance(other, Vector):
-            return NotImplemented
-        return self.x == other.x and self.y == other.y
 
 
 class SnakeGame:
